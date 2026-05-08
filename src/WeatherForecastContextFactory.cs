0fece4e95640c1bdab62578b0064f7677036656d
renldx <renldx@gmail.com>
2026-05-08 18:36:10 -0400

    Fix misc. issues

---
+++ src/WeatherForecastContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using WebApi.Models;
+
+namespace WebApi
+{
+    public class WeatherForecastContextFactory : IDesignTimeDbContextFactory<WeatherForecastContext>
+    {
+        public WeatherForecastContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WeatherForecastContext>();
+
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=design_time_db;Trusted_Connection=True;");
+
+            return new WeatherForecastContext(optionsBuilder.Options);
+        }
+    }
+}
